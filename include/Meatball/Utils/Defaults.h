c243c8d5cdf95884ff75c6a4ae0ae4b16373b148
Suado Cowboy <luccacsg@gmail.com>
2024-08-31 12:01:25 -0300

    using latest meatball version

--- include/Meatball/Utils/Defaults.h
+++ include/Meatball/Utils/Defaults.h
@@ -9,6 +9,7 @@
 
 namespace Meatball::Defaults {
     /// @brief loads all the needed data
+    /// @param defaultFont if the json file contains a valid defaultFont path, this will change or else it will be whatever was set before
     void init(const std::string& jsonPath, Font& defaultFont);
 
     void loadConsoleFonts(ConsoleUI& consoleUI, const std::filesystem::path& fontPath, Font& outGeneralFont, Font& labelFont);
