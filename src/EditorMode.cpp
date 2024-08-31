58352ebab60d762c34d0c35a7a18e01ecd2f1824
Suado Cowboy <luccacsg@gmail.com>
2024-08-31 20:25:38 -0300

    improved EditorMode

--- src/EditorMode.cpp
+++ src/EditorMode.cpp
@@ -1,3 +1,52 @@
 #include "EditorMode.h"
 
-bool uiEditorMode = true;
+#include <Meatball/Utils/Defaults.h>
+#include <Meatball/Utils/DrawFuncs.h>
+
+bool editorMode = true;
+
+Editor::Editor() {
+    toolsPanel.config = &Defaults::dynamicPanelConfig;
+    toolsPanelScrollBar.config = &Defaults::scrollBarConfig;
+
+    toolsPanel.rect.width = GetScreenWidth()*0.25f;
+    toolsPanel.rect.height = GetScreenHeight();
+
+    toolsPanel.rect.x = GetScreenWidth()-toolsPanel.rect.width;
+    toolsPanel.rect.y = 0.0f;
+
+    toolsPanel.onMove = [&]() {
+        toolsPanelScrollBar.setPosition(
+            toolsPanel.rect.x+toolsPanel.rect.width-toolsPanelScrollBar.getRect().width,
+            toolsPanel.rect.y
+        );
+    };
+
+    toolsPanel.onResize = [&]() {
+        toolsPanelScrollBar.setSize(toolsPanel.rect.width*0.25f, toolsPanel.rect.height);
+
+        toolsPanel.onMove();
+    };
+
+    toolsPanel.onResize();
+}
+
+void Editor::update() {
+    toolsPanel.update();
+    toolsPanelScrollBar.update(toolsPanel.rect);
+}
+
+void Editor::draw() {
+    drawRect(toolsPanel.rect, toolsPanel.config->color);
+    toolsPanelScrollBar.draw();
+}
+
+void Editor::onResize(float ratioWidth, float ratioHeight) {
+    toolsPanel.rect.x *= ratioWidth;
+    toolsPanel.rect.y *= ratioHeight;
+
+    toolsPanel.rect.width *= ratioWidth;
+    toolsPanel.rect.height *= ratioHeight;
+    
+    toolsPanel.onResize();
+}
--- src/EditorMode.h
+++ src/EditorMode.h
@@ -1,3 +1,22 @@
 #pragma once
 
-extern bool uiEditorMode;
+#include <Meatball/IScene.h>
+#include <Meatball/Interface/DynamicPanel.h>
+#include <Meatball/Interface/ScrollBar.h>
+
+extern bool editorMode;
+
+using namespace Meatball;
+
+class Editor : public IScene {
+public:
+    Editor();
+
+    void update();
+    void draw();
+
+    void onResize(float ratioWidth, float ratioHeight);
+
+    DynamicPanel toolsPanel;
+    ScrollBar toolsPanelScrollBar;
+};
